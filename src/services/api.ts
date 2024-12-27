b0786aeef1f8d02bdfa8f607bc712b616699a367
gabriel.kaminise <gabriel.kaminise@sankhya.com.br>
2024-12-27 15:37:51 -0300

    Ajustes

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://controledeclientes-production.up.railway.app', // Substitua pela URL do backend
+    baseURL: 'https://controledeclientes-production.up.railway.app', // Substitua pela URL do backend
 });
 
 export default api;
